b8d28ead6b5748218df3f5b9ac6edec1a276bd79
gshinecomp <g.shine.comp@gmail.com>
2018-07-12 01:51:28 +0800

    fix image resize for emacs 25.3.1

--- init.el
+++ init.el
@@ -76,6 +76,7 @@ Return the updated `exec-path'"
 ;; -------------------------------------------------------------------
 ;; from https://emacs-china.org/t/topic/1933/48
 ;; Emacs 25.2.1, Windows7 32bit, Org 9.0.8. 亲测可用。 在Emacs的环境变量里加上Imagemagick（我使用的是它的便携版，官网下载，里面必需包括mogrify.exe程序），然后把jkitchin的程序代码写在配置中，就可以对图片进行缩放了。
+;; fix by js.wang(Erik), modified to Emacs 25.3.1 + ImageMagick-7.0.8-6-portable-Q16-x86 (use mogrify)
 
 (with-eval-after-load 'org
 ;; * Rescaling inline-images
@@ -120,16 +121,21 @@ See http://www.imagemagick.org/Usage/resize/#resize for more options."
 ;; this is copied and modified from org.el
 (defun org-display-inline-images (&optional include-linked refresh beg end)
   "Display inline images.
+
 An inline image is a link which follows either of these
 conventions:
+
   1. Its path is a file with an extension matching return value
      from `image-file-name-regexp' and it has no contents.
+
   2. Its description consists in a single link of the previous
      type.
+
 When optional argument INCLUDE-LINKED is non-nil, also links with
 a text description part will be inlined.  This can be nice for
 a quick look at those images, but it does not reflect what
 exported files will look like.
+
 When optional argument REFRESH is non-nil, refresh existing
 images between BEG and END.  This will create new image displays
 only if necessary.  BEG and END default to the buffer
@@ -141,19 +147,28 @@ boundaries."
       (when (fboundp 'clear-image-cache) (clear-image-cache)))
     (org-with-wide-buffer
      (goto-char (or beg (point-min)))
-     (let ((case-fold-search t)
-	   (file-extension-re (image-file-name-regexp)))
-       (while (re-search-forward "[][]\\[\\(?:file\\|[./~]\\)" end t)
+     (let* ((case-fold-search t)
+	    (file-extension-re (image-file-name-regexp))
+	    (link-abbrevs (mapcar #'car
+				  (append org-link-abbrev-alist-local
+					  org-link-abbrev-alist)))
+	    ;; Check absolute, relative file names and explicit
+	    ;; "file:" links.  Also check link abbreviations since
+	    ;; some might expand to "file" links.
+	    (file-types-re (format "[][]\\[\\(?:file\\|[./~]%s\\)"
+				   (if (not link-abbrevs) ""
+				     (format "\\|\\(?:%s:\\)"
+					     (regexp-opt link-abbrevs))))))
+       (while (re-search-forward file-types-re end t)
 	 (let ((link (save-match-data (org-element-context))))
-	   ;; Check if we're at an inline image.
-	   (when (and (equal (org-element-property :type link) "file")
+	   ;; Check if we're at an inline image, i.e., an image file
+	   ;; link without a description (unless INCLUDE-LINKED is
+	   ;; non-nil).
+	   (when (and (equal "file" (org-element-property :type link))
 		      (or include-linked
-			  (not (org-element-property :contents-begin link)))
-		      (let ((parent (org-element-property :parent link)))
-			(or (not (eq (org-element-type parent) 'link))
-			    (not (cdr (org-element-contents parent)))))
-		      (org-string-match-p file-extension-re
-					  (org-element-property :path link)))
+			  (null (org-element-contents link)))
+		      (string-match-p file-extension-re
+				      (org-element-property :path link)))
 	     (let ((file (expand-file-name
 			  (org-link-unescape
 			   (org-element-property :path link)))))
@@ -161,64 +176,62 @@ boundaries."
 		 (let ((width
 			;; Apply `org-image-actual-width' specifications.
 			(cond
+
+            ;; fix for image resize
 			 ((and (not (image-type-available-p 'imagemagick))
 			       (not org-inline-image-resize-function))
 			  nil)
+
 			 ((eq org-image-actual-width t) nil)
 			 ((listp org-image-actual-width)
 			  (or
 			   ;; First try to find a width among
 			   ;; attributes associated to the paragraph
 			   ;; containing link.
-			   (let* ((paragraph
-				   (let ((e link))
-				     (while (and (setq e (org-element-property
-							  :parent e))
-						 (not (eq (org-element-type e)
-							  'paragraph))))
-				     e))
-				  (attr_org (org-element-property :attr_org paragraph)))
-			     (when attr_org
-			       (plist-get
-				(org-export-read-attribute :attr_org  paragraph) :width)))
+			   (let ((paragraph
+				  (let ((e link))
+				    (while (and (setq e (org-element-property
+							 :parent e))
+						(not (eq (org-element-type e)
+							 'paragraph))))
+				    e)))
+			     (when paragraph
+			       (save-excursion
+				 (goto-char (org-element-property :begin paragraph))
+				 (when
+				     (re-search-forward
+				      "^[ \t]*#\\+attr_.*?: +.*?:width +\\(\\S-+\\)"
+				      (org-element-property
+				       :post-affiliated paragraph)
+				      t)
+				   (string-to-number (match-string 1))))))
 			   ;; Otherwise, fall-back to provided number.
 			   (car org-image-actual-width)))
 			 ((numberp org-image-actual-width)
 			  org-image-actual-width)))
 		       (old (get-char-property-and-overlay
 			     (org-element-property :begin link)
-			     'org-image-overlay))) 
+			     'org-image-overlay)))
 		   (if (and (car-safe old) refresh)
 		       (image-refresh (overlay-get (cdr old) 'display))
 		     
+            ;; add for image resize
 		     (when org-inline-image-resize-function
 		       (setq file (funcall  org-inline-image-resize-function file width)
 			     width nil))
+
 		     (let ((image (create-image file
-						(cond
-						 ((image-type-available-p 'imagemagick)
-						  (and width 'imagemagick))
-						 (t nil)) 
+						(and width 'imagemagick)
 						nil
 						:width width)))
 		       (when image
-			 (let* ((link
-				 ;; If inline image is the description
-				 ;; of another link, be sure to
-				 ;; consider the latter as the one to
-				 ;; apply the overlay on.
-				 (let ((parent
-					(org-element-property :parent link)))
-				   (if (eq (org-element-type parent) 'link)
-				       parent
-				     link)))
-				(ov (make-overlay
-				     (org-element-property :begin link)
-				     (progn
-				       (goto-char
-					(org-element-property :end link))
-				       (skip-chars-backward " \t")
-				       (point)))))
+			 (let ((ov (make-overlay
+				    (org-element-property :begin link)
+				    (progn
+				      (goto-char
+				       (org-element-property :end link))
+				      (skip-chars-backward " \t")
+				      (point)))))
 			   (overlay-put ov 'display image)
 			   (overlay-put ov 'face 'default)
 			   (overlay-put ov 'org-image-overlay t)
