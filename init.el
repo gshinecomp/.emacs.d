d08db95e54e6f77c78142203c149645d6d3eb71c
gshinecomp <g.shine.comp@gmail.com>
2018-07-11 12:28:18 +0800

    first version
    emacs initize file first version

---
+++ init.el
@@ -0,0 +1,5 @@
+;; package.el 相關設定
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
